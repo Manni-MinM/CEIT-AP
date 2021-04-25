62d218ef93217e4717e96bcd0aa5ccfd005efcb9
manni-minm <mannimoghimi@gmail.com>
2021-04-25 17:53:51 +0430

    finished Pile

--- HW4/P3/Main.java
+++ HW4/P3/Main.java
@@ -2,13 +2,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Card card1 = new NormalCard("3" , "RED") ;
-		Card card2 = new ForceCard("2" , "BLACK") ;
-		
-		System.out.println(card1.toString()) ;
-		System.out.println(card2.toString()) ;
-		
-		System.out.println(card2.getColor()) ;
-		System.out.println(card1.equals(card2)) ;
+		Pile pile = new Pile() ;
+		pile.show() ;
 	}
 }
--- HW4/P3/Pile.java
+++ HW4/P3/Pile.java
@@ -1,13 +1,59 @@
 // BWOTSHEWCHB
 
 import java.util.ArrayList ;
+import java.util.Collections ;
 
 public class Pile {
 	// Fields
-	ArrayList<Card> Pile ;
+	ArrayList<Card> pile ;
 	// Constructor
 	public Pile() {
-		Pile = new ArrayList<Card>() ;
+		pile = new ArrayList<Card>() ;
+
+		String[] normalCards = {"3" , "4" , "5" , "6" , "9" , "C" , "D"} ; 
+		for ( String rank : normalCards ) {
+			pile.add(new NormalCard(rank , "RED")) ;
+			pile.add(new NormalCard(rank , "GREEN")) ;
+			pile.add(new NormalCard(rank , "BLUE")) ;
+			pile.add(new NormalCard(rank , "BLACK")) ;
+		}
+
+		pile.add(new ForceCard("2" , "RED")) ;
+		pile.add(new ForceCard("2" , "GREEN")) ;
+		pile.add(new ForceCard("2" , "BLUE")) ;
+		pile.add(new ForceCard("2" , "BLACK")) ;
+
+		pile.add(new DrawFourCard("7" , "BLACK")) ;
+
+		pile.add(new DrawTwoCard("7" , "RED")) ;
+		pile.add(new DrawTwoCard("7" , "GREEN")) ;
+		pile.add(new DrawTwoCard("7" , "BLUE")) ;
+
+		pile.add(new PrizeCard("8" , "RED")) ;
+		pile.add(new PrizeCard("8" , "GREEN")) ;
+		pile.add(new PrizeCard("8" , "BLUE")) ;
+		pile.add(new PrizeCard("8" , "BLACK")) ;
+
+		pile.add(new ReverseCard("10" , "RED")) ;
+		pile.add(new ReverseCard("10" , "GREEN")) ;
+		pile.add(new ReverseCard("10" , "BLUE")) ;
+		pile.add(new ReverseCard("10" , "BLACK")) ;
+
+		pile.add(new SkipCard("A" , "RED")) ;
+		pile.add(new SkipCard("A" , "GREEN")) ;
+		pile.add(new SkipCard("A" , "BLUE")) ;
+		pile.add(new SkipCard("A" , "BLACK")) ;
+
+		pile.add(new WildCard("B" , "RED")) ;
+		pile.add(new WildCard("B" , "GREEN")) ;
+		pile.add(new WildCard("B" , "BLUE")) ;
+		pile.add(new WildCard("B" , "BLACK")) ;
+
+		Collections.shuffle(pile) ;
+	}
+	public void show() {
+		for ( Card card : pile )
+			System.out.println(card.toString()) ;
 	}
 }
 
