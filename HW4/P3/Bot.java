4795caee40bca1ad224ca55f02f4dc14136902c6
manni-minm <mannimoghimi@gmail.com>
2021-04-27 17:07:11 +0430

    added Javadoc for some classes

--- HW4/P3/Bot.java
+++ HW4/P3/Bot.java
@@ -3,14 +3,30 @@
 import java.util.ArrayList ;
 import java.util.Collections ;
 
+/**
+ * Class to Hold information about Bots
+ *
+ * @author Manni Moghimi
+ * @version v1.0
+ */
 public class Bot extends Player {
-	// Fields
-
 	// Constructor
+	/**
+	 * Creates Bot Object
+	 *
+	 * @param username Username of the Bot
+	 */
 	public Bot(String username) {
 		super(username) ;
 	}
 	// Methods
+	/**
+	 * Chooses a card and plays it
+	 * Finds all avaliable cards for play and adds it to a list then chooses one from it at random
+	 *
+	 * @param topCard Top card which is in play now 
+	 * @return If a card can be played returns the card otherwise returns null
+	 */
 	public Card playCard(Card topCard) {
 		// Find avaliable cards
 		ArrayList<Card> avaliable = new ArrayList<Card>() ;
@@ -20,10 +36,16 @@ public class Bot extends Player {
 		if ( avaliable.size() == 0 )
 			return null ;
 		// Get info
+		Collections.shuffle(avaliable) ;
 		Card targetCard = avaliable.get(0) ;
 		deck.removeCard(targetCard) ;
 		return targetCard ;
 	}
+	/**
+	 * Chooses a color at random after using the wildcard
+	 *
+	 * @return The color chosen at random
+	 */
 	public String getWildCardColor() {
 		ArrayList<String> colors = new ArrayList<String>() ;
 		colors.add("RED") ;
--- HW4/P3/Card.java
+++ HW4/P3/Card.java
@@ -2,14 +2,21 @@
 
 import java.util.ArrayList ;
 
+/**
+ * An abstract Class to hold information about Cards
+ *
+ * @author Manni Moghimi
+ * @version v1.0
+ */
 public abstract class Card {
 	// Fields
 	protected int point ;
 	protected String rank ;
 	protected String color ;
-	// Constructor
-	
 	// Classes
+	/**
+	 * A Class to hold information about ANSI escape code colors
+	 */
 	public class Color {
 		// Fields
 		public static final String ANSI_RESET = "\u001B[0m";
@@ -22,6 +29,11 @@ public abstract class Card {
 		public static final String ANSI_CYAN = "\u001B[36m";
 		public static final String ANSI_WHITE = "\u001B[37m";
 		// Methods
+		/**
+		 * Returns escape code of given color
+		 *
+		 * @param color Name of the color
+		 */
 		public String getANSI(String color) {
 			if ( color.equals("RED") )
 				return ANSI_RED ;
@@ -35,15 +47,37 @@ public abstract class Card {
 		}
 	}
 	// Methods
+	/**
+	 * Returns the Point of the Card
+	 *
+	 * @return The Point of the Card 
+	 */
 	public int getPoint() {
 		return point ;
 	}
+	/**
+	 * Returns the Rank of the Card
+	 *
+	 * @return The Rank of the Card
+	 */
 	public String getRank() {
 		return rank ;
 	}
+	/**
+	 * Returns the Color of the Card
+	 *
+	 * @return The Color of the Card
+	 */
 	public String getColor() {
 		return color ;
 	}
+	/**
+	 * Checks if two Object are equal
+	 * 
+	 * @param object the Object to be checked
+	 * @return True if they are equal otherwise False
+	 */
+	@Override
 	public boolean equals(Object object) {
 		if ( !(object instanceof Card) )
 			return false ;
@@ -52,12 +86,29 @@ public abstract class Card {
 		Card card = (Card)object ;
 		return (this.getRank().equals(card.getRank()) && this.getColor().equals(card.getColor())) ;
 	}
+	/**
+	 * Converts Object of Card type to String format
+	 *
+	 * @return The Card in String format
+	 */
+	@Override
 	public String toString() {
 		return (this.getRank() + "-" + this.getColor()) ;
 	}
+	/**
+	 * Checks if the next Card can be played after the current card
+	 *
+	 * @param nextCard the nextCard which we want to play
+	 * @return True if nextCard can be played after the current Card otherwise False
+	 */
 	public boolean isValid(Card nextCard) {
 		return (this.getRank().equals(nextCard.getRank()) || this.getColor().equals(nextCard.getColor()) || nextCard.getRank().equals("B")) ;
 	}
+	/**
+	 * Converts the Card to a somewhat graphical format which can be displayed in the Terminal
+	 *
+	 * @return A list containing the lines of the sorta graphical Card
+	 */
 	public ArrayList<String> stringed() {
 		ArrayList<String> line = new ArrayList<String>() ;
 		Color paint = new Color() ;
@@ -86,6 +137,11 @@ public abstract class Card {
 		}
 		return line ;
 	}
+	/**
+	 * Converts the Card to a somewhat graphical format which can be displayed in the Terminal
+	 *
+	 * @return A list containing the lines of the sorta graphical half Card
+	 */
 	public ArrayList<String> stringedWithoutRightSide() {
 		ArrayList<String> line = new ArrayList<String>() ;
 		Color paint = new Color() ;
